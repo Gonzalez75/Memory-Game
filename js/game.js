d36161341e4118b6279a2778d5b514ca487b12e2
Gonzalez75 <guilherme75gonzalez@gmail.com>
2022-06-28 13:34:00 -0300

    commit #2

---
+++ js/game.js
@@ -0,0 +1,16 @@
+function createCard (){
+    const card = document.createElement('div');
+    const front = document.createElement('div');
+    const back = document.createElement('div');
+
+    card.className = 'card';
+    front.className = 'face front';
+    back.className = 'face back';
+
+    card.appendChild(front);
+    card.appendChild(back);
+
+    grid.appendChild(card);
+}
+
+createCard()
