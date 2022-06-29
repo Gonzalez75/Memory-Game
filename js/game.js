36731ac6d0a50a9b6c8099c97b53ffe8fcfa9592
Gonzalez75 <guilherme75gonzalez@gmail.com>
2022-06-29 20:42:49 -0300

    commit #3

--- js/game.js
+++ js/game.js
@@ -1,16 +1,110 @@
-function createCard (){
-    const card = document.createElement('div');
-    const front = document.createElement('div');
-    const back = document.createElement('div');
+const grid = document.querySelector('.grid');
 
-    card.className = 'card';
-    front.className = 'face front';
-    back.className = 'face back';
+const characters = [
+    'bulbassaur',
+    'charmander',
+    'eevee',
+    'gengar',
+    'jigglypuff',
+    'magikarp',
+    'meowth',
+    'pikachu',
+    'snorlax',
+    'squirtle',
+];
 
-    card.appendChild(front);
-    card.appendChild(back);
-
-    grid.appendChild(card);
+const createElement = (tag, className) => {
+  const element = document.createElement(tag);
+  element.className = className;
+  return element;
 }
 
-createCard()
+let firstCard = '';
+let secondCard = '';
+
+const checkEndGame = () => {
+  const disabledCards = document.querySelectorAll('.disabled-card');
+
+  if (disabledCards.length === 20) {
+    alert('Parabéns, você conseguiu!');
+  }
+}
+
+const checkCards = () => {
+  const firstCharacter = firstCard.getAttribute('data-character');
+  const secondCharacter = secondCard.getAttribute('data-character');
+
+  if (firstCharacter === secondCharacter) {
+
+    firstCard.firstChild.classList.add('disabled-card');
+    secondCard.firstChild.classList.add('disabled-card');
+
+    firstCard = '';
+    secondCard = '';
+
+    checkEndGame();
+
+  } else {
+    setTimeout(() => {
+
+      firstCard.classList.remove('reveal-card');
+      secondCard.classList.remove('reveal-card');
+
+      firstCard = '';
+      secondCard = '';
+
+    }, 500);
+  }
+
+}
+
+const revealCard = ({ target }) => {
+
+  if (target.parentNode.className.includes('reveal-card')) {
+    return;
+  }
+
+  if (firstCard === '') {
+
+    target.parentNode.classList.add('reveal-card');
+    firstCard = target.parentNode;
+
+  } else if (secondCard === '') {
+
+    target.parentNode.classList.add('reveal-card');
+    secondCard = target.parentNode;
+
+    checkCards();
+
+  }  
+}
+
+const createCard = (character) => {
+
+  const card = createElement('div', 'card');
+  const front = createElement('div', 'face front');
+  const back = createElement('div', 'face back');
+
+  front.style.backgroundImage = `url('../images/${character}.jpg')`;
+
+  card.appendChild(front);
+  card.appendChild(back);
+
+  card.addEventListener('click', revealCard);
+  card.setAttribute('data-character', character)
+
+  return card;
+}
+
+const loadGame = () => {
+  const duplicateCharacters = [ ...characters, ...characters ];
+
+  const shuffledArray = duplicateCharacters.sort(() => Math.random() - 0.5);
+
+  shuffledArray.forEach((character) => {
+    const card = createCard(character);
+    grid.appendChild(card);
+  });
+}
+
+loadGame();
